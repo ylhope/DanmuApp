38883a8b25baf45d18920c6917e5b49f98107f68
ylhope <858131544@qq.com>
2017-08-02 22:23:20 +0800

    difpage
    写在不同文件夹里

---
+++ js/mainpage.js
@@ -0,0 +1,12 @@
+//$(document).ready(function(){
+	function input(){
+    var text = document.getElementById("inputtext").value;
+    //output.value = text;
+    // document.write(text); 
+     $("#output").html(text);
+    };
+
+	function clean(){
+		document.write(text); 
+	}
+//})
